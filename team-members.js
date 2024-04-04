fd3b5459f4d546556eb6721591c1e1982783d10a
Rohit Gupta <rohitgupta@Rohits-MacBook-Air.local>
2024-04-04 12:12:16 -0400

    team-members js file added

---
+++ team-members.js
@@ -0,0 +1,133 @@
+const memberQuotes = {
+  abigale:
+    "Abigale specializes in creative content direction and creation, helping brands elevate their storytelling through creating UGC, product photography and videography, graphic design, short form video, and social media management.",
+  alisha:
+    "Alisha is a well seasoned copywriter with an impressive portfolio that includes accomplished brands like Samsung Global, Amazon, Google, Sephora UAE, Tom Ford, and more. Known for her ability to craft compelling narratives, Alisha takes pride in immersing herself in each brand she works with, ensuring that every word she pens aligns seamlessly with your identity. She doesn't merely write, she curates words strategically with the primary goal of selling.",
+  alycia:
+    "Alycia is a designer with a background in elevating direct-to-consumer (DTC) brands from beauty to technology. With a keen eye for innovation and design, she brings a dynamic approach to crafting engaging and marketable brands.",
+  amber:
+    "Amber is a senior designer with over a decade of expertise in the industry. Her design approach is characterized by curiosity, empathy, and a clear sense of purpose. Having navigated the challenges of in-house roles at a DTC startup, Amber brings a unique understanding of the dynamics on the brand side.",
+  ari: "Ari oversees Growth for all clients - leading our Paid, CRM, CRO, and Account Teams. A trained ecommerce manager, she comes to Sharma Brands from about-face beauty by Halsey, Poosh by Kourtney Kardashian, Pressed Juicery, and SnackNation. Ari also writes Go-to-Millions, an ecommerce ~newsletter~ love-letter read by over 50,000 DTC marketers.",
+  ashan:
+    "Ashan, a seasoned Performance Marketer, orchestrates media buying efforts for our clients at Sharma Brands with finesse and precision.",
+  bailee:
+    "Bailee manages the Internal Brands arm at Sharma Brands, paying special attention to the growth, marketing and product development of the companies.",
+  berkeley:
+    "Berkeley works as a point of contact for our clients and manages ongoing projects to ensure efficient communication, good vibes and timely deliveries. ✨",
+  bilal: "",
+  binoy:
+    "Binoy handles operational projects and tasks for the design and creative teams. He assists the Creative Director and the Design Director in identifying opportunities to increase efficiency in internal dynamics and day-to-day operations.",
+  brandon:
+    "Brandon is a Video Editor and Animator that prioritizes engaging movement for thumb-stopping content. Having had a hand in every aspect of the creative process, Brandon always keeps fresh ideas flowing and always pushes the bounds of what a creative and engaging ad looks like.",
+  carly:
+    "Carly manages a team of video editors, animators and graphic designers that are experts in performance-driven, direct-response ad creative. With experience working across top-tier brands in the DTC space, and alongside the Paid Team, she shapes, strategizes, and leads the execution of the creative marketing vision for all Sharma Brands clients.",
+  chandler:
+    "Chandler is a copywriter and content strategist with over 4.5 years of marketing and writing experience. She’s worked in-house for several fast-growing startups and has also freelanced for brands like Rent the Runway, Cann Social Tonics, JAJA Tequila, and more. When she’s not writing, she can likely be found running across Mexico City, trying (failing) to learn Spanish, or devouring a bag of Cheetos.",
+  christina:
+    "Christina is a master of performance content who holds herself and everyone around her to a high standard. She makes sure that every ad we deliver is high-quality and effective.",
+  devender:
+    "From simple tweaks to complex development, Devender can do it. He has a knack for solving e-commerce problems.",
+  gunner:
+    "Gunner runs all marketing efforts for Sharma Brands and Nik’s personal newsletter. He started his career at 17 as one of the core team members at Grailed. Over time he honed his copywriting skills and made his foray into DTC as a Content Strategist at GeistM. He eventually fell in love with direct response during his time at VaynerCommerce, exploring everything across acquisition and retention as a Growth Analyst.",
+  isabel:
+    "As a video editor, Isabel creates engaging, bite-sized stories to get people interested in learning more about the product or service being showcased. She is always ready to take on the challenge of finding the balance between fun, exciting visuals and informative, memorable messaging.",
+  jake: "Jake ensures all steps of the paid media process are running cost effectively and to the brand's standard. Everything from building to performance reporting.",
+  james:
+    "James is a video editor and animator with over 10 years of experience. He specializes in taking brand stories and forming them into engaging video ads.",
+  jaz: "Jaz fell in love with ecommerce during her time as a product designer at Shopify. In her downtime, she launched and scaled a DTC skincare brand out of her kitchen with just $1000 and a credit card. At Sharma Brands, Jaz uses her expertise to lead the Design team, creating intuitive experiences that are not only beautiful, but crafted with conversion and revenue goals in mind.",
+  jeremy:
+    "Jeremy designs wireframes that involve copywriting, structure, and the overall flow for landing pages (and occasionally websites). He specializes in conducting user research, leveraging eye tracking and page interaction analysis to craft pages aimed at optimizing conversion rates with the users top of mind.",
+  julia:
+    "Julia conducts user research in order to create wireframes and designs that fit both your brand’s and your customers’ needs. Before switching careers to become a designer, Julia was an educator of 10+ years, and now uses her knowledge of designing lessons and experiences for students and other educators in her UX/UI work today.",
+  lenny:
+    "With an eye on your profit, a hand in CRO, and a plan for the best-in-class tech stack, Lenny is your business's go-to growth engineer.",
+  maria:
+    "With a love of storytelling and consumer behavior, Maria works hand in hand with our Retention Director on all things CRM for our portfolio of DTC clients. Maria is a senior digital marketer with 6 years of experience working on email, SMS, and social content + strategy for high growth DTC brands and agencies in the CPG, apparel, and wellness spaces.",
+  matt: "With a passion for turning stats into success stories, Matt spearheads user research, A/B testing, landing page performance and reporting. With a focus on data-driven strategies, Matt plays a crucial role in optimizing the customer journey for unparalleled success.",
+  meredith:
+    "Meredith oversees strategy and operations as Chief Operating Officer. She started her career at Tiffany & Co. in global marketing, merchandising, and eCommerce before moving into the DTC startup space where she led award-winning marketing and creative teams at NAADAM and For Days.",
+  merin:
+    "Merin oversees and manages all areas of the client scopes and relationships, ensuring all projects are being delivered to the client in a timely and efficient manner. Serving as the primary point of contact, Merin maintains a close relationship with our clients, ensuring seamless communication and addressing all needs efficiently.",
+  minul:
+    "Minul works closely with the COO of Sharma Brands, tackling the worlds of Sales Ops, HR, and Finance together. It's a rollercoaster of strategy and success.",
+  mohit:
+    "Crafting digital excellence with code, Mohit turns visions into seamless online experiences at Sharma Brands",
+  morgan:
+    "Morgan engages deeply across development and design on client scopes specializing in Brand Site Launches, Site Redesigns, CRO, and CRM. With previous agency experience as Director of Content at GeistM, Morgan brings a 360 degree view of ecom to the team.",
+  natalia: "",
+  nero: "Nero, a CRM team member, specializes in email design, focusing on boosting conversion rates and improving user experience. With a background in product design and graphics, she seamlessly blends visual storytelling to craft captivating email campaigns that drive conversions.",
+  nina: "As retention director at Sharma Brands, Nina oversees all CRM strategy and campaign rollouts for our portfolio of DTC clients - ensuring that they are reaching the right people, at the right times, and keep them coming back for more. Nina is a seasoned growth marketer with over 8 years of experience working at high-impact creative agencies, and has led retention efforts for brands like Feastables, Meghan Fabulous, TILIT NYC, FAWN Design, and Cowboy Pools.",
+  rameez:
+    "As the right hand to Nik at Sharma Brands, Rameez manages everything necessary to keep the focus on forward momentum. His role encompasses a broad range of responsibilities, from logistical details to ensuring smooth operations, all tailored to support the leadership’s vision.",
+  rohit:
+    "Rohit is an award-winning computer wizard who specializes in many different components of the development and retention lifecycle. His goal is to marry the complexity of code with the beauty and aesthetic of quality design to create a gorgeous and innovative end-product that demands attention and brings in the $$$!",
+  taylor:
+    "Taylor loves to combine his passion for design, motion, and video all together. You can’t beat beautiful content that drives results!",
+  vicky:
+    "Vicky designs static ads on the performance creative team. With a background in UX and visual design, she specializes in tying the two together to deliver scroll-stopping ads. She loves researching about brand stories to find balance between visual aesthetic and important information hierarchies.",
+};
+
+var teamMembers = document.getElementsByClassName("team-member");
+var popupModalBg = document.getElementById("popup-modal-bg");
+var modalContent = document.getElementById("modal-content");
+var modalName = document.getElementById("modal-team-member-name");
+var modalDesc = document.getElementById("modal-team-member-description");
+
+for (let mem of teamMembers) {
+  let member = mem.id;
+  let memberName = mem.getElementsByClassName("name")[0].innerText;
+  let memberPic = mem.getElementsByClassName("team-member-pic")[0];
+
+  memberPic.addEventListener("click", () => {
+    modalName.innerText = `About ${memberName}`;
+
+    // Split the text into parts based on the squiggly lines (~~)
+    let parts = memberQuotes[member].split("~~");
+    let finalString = "";
+
+    // Iterate through the parts and apply strikethrough if it's between squiggly lines
+    for (let i = 0; i < parts.length; i++) {
+      if (i % 2 === 0) {
+        // Regular text
+        finalString += parts[i];
+      } else {
+        // Text between squiggly lines
+        finalString += `<s>${parts[i]}</s>`;
+      }
+    }
+
+    modalDesc.innerText = finalString;
+
+    popupModalBg.style.display = "block";
+    modalContent.style.display = "block";
+
+    popupModalBg.style.opacity = "0.5";
+    modalContent.style.opacity = "1";
+
+    function closeModal() {
+      popupModalBg.style.opacity = "0";
+      modalContent.style.opacity = "0";
+
+      popupModalBg.style.display = "none";
+      modalContent.style.display = "none";
+
+      return;
+    }
+
+    var closeButton = document.getElementById("modal-close-button");
+
+    window.addEventListener("keydown", function (e) {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    });
+
+    popupModalBg.addEventListener("click", () => {
+      closeModal();
+    });
+
+    closeButton.addEventListener("click", () => {
+      closeModal();
+    });
+  });
+}
